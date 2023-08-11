3ed8c3a5f55611fcc87da2d89456c6a23e5ceb74
bilson15 <gabrielfelipe.bil@gmail.com>
2023-08-11 11:07:46 -0300

    fisrt commit

---
+++ src/main/java/com/psitime/PsitimeApiApplication.java
@@ -0,0 +1,13 @@
+package com.psitime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsitimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsitimeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psitime/PsitimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psitime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsitimeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
